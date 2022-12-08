389d2c065a84be070bcf86fe55a7612786c20c44
Axelle-SKICA <axelle.skica@gmail.com>
2022-12-08 20:25:43 +0100

    client sequelize pour connexion à la BDD postgreSQL

---
+++ app/client-sequelize.js
@@ -0,0 +1,14 @@
+require("dotenv/config");
+
+const { Sequelize } = require('sequelize');
+
+// instance de connexion à la BDD Postgres
+const sequelize = new Sequelize(`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+  dialect: "postgres",
+  define: {
+    createdAt: "created_at", 
+    updatedAt: "updated_at" 
+  }
+});
+
+module.exports = sequelize;
