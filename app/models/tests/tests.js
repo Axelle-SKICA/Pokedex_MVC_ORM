1decd8553ac89346d1d1b081120cfe8167b6346d
Axelle-SKICA <axelle.skica@gmail.com>
2022-12-08 20:58:55 +0100

    plus de tests des Models dans tests.js

--- app/models/tests/tests.js
+++ app/models/tests/tests.js
@@ -1,13 +1,45 @@
 const { Pokemon, Type } = require('../index');
 
+//afficher en console l'ensemble des Pokemons
 async function test01() {
     try {
         const pokemonsList = await Pokemon.findAll();
         console.log(JSON.stringify(pokemonsList, null, 2));
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error.message);
     }
 }
 
-test01();
+// test01();
+
+//afficher en console le pokemon selon son id, avec son/ses type(s)
+async function test02(id) {
+    try {
+        const searchedPokemonWithType = await Pokemon.findByPk(id, {
+            include: "types",
+        });
+        console.log(searchedPokemonWithType.get({ plain: true }));
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+test02(25);
+
+
+//afficher en console le Type Psy avec les pokemons de ce type:
+async function test03() {
+    try {
+        const psyTypeWithPokemons = await Type.findOne({
+            where: {
+                name: "Psy"
+            },
+            include: "pokemons"
+        });
+        console.log(psyTypeWithPokemons.get({ plain: true }));
+    } catch (error) {
+        console.log(error.message)
+    }
+};
+
+// test03();
