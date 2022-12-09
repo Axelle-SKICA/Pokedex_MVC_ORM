9f10d2dca598c0178b3208656f0ce7a7986f5e4a
Axelle-SKICA <axelle.skica@gmail.com>
2022-12-09 18:00:55 +0100

    début construction view pokemonDetails + css associé

--- app/controllers/pokemonsController.js
+++ app/controllers/pokemonsController.js
@@ -3,6 +3,7 @@ const {Pokemon, Type} = require('../models/index')
 const pokemonsController = {
     getPokemonsPage: async (req, res) => {
         try {
+            //on récupère en DB la liste de tous les pokemons:
             const pokemons = await Pokemon.findAll();
             res.render("pokemons", {pokemons});
         } catch (error) {
@@ -12,10 +13,11 @@ const pokemonsController = {
     },
 
     getOnePokemonPage: async (req, res) => {
-        //on récupère l'id du pokemon dans l'url de la route paramétrée
+        //on récupère l'id du pokemon dans l'url de la route paramétrée:
         const searchedId = Number(req.params.id);
         
         try {
+            //on récupère en DB le pokemon (et ses types) dont l'id correspond à searchedId:
             const searchedPokemon = await Pokemon.findByPk(searchedId, {
                 include: "types"
             });
